755880ea2ef47a9ebf11bea8903a7b49aff06979
enunsnv <seoej0315@naver.com>
2026-03-29 02:47:58 +0900

    📝 feat: 한글 utils 추가

---
+++ tadak/app/utils/hangul.ts
@@ -0,0 +1,28 @@
+export const decompose = (char: string) => {
+  const code = char.charCodeAt(0) - 44032;
+
+  if (code < 0 || code > 11171) return [char];
+
+  const cho = Math.floor(code / 588);
+  const jung = Math.floor((code % 588) / 28);
+  const jong = code % 28;
+
+  const CHO = [
+    "ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ",
+    "ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"
+  ];
+  const JUNG = [
+    "ㅏ","ㅐ","ㅑ","ㅒ","ㅓ","ㅔ","ㅕ","ㅖ","ㅗ","ㅘ",
+    "ㅙ","ㅚ","ㅛ","ㅜ","ㅝ","ㅞ","ㅟ","ㅠ","ㅡ","ㅢ","ㅣ"
+  ];
+  const JONG = [
+    "","ㄱ","ㄲ","ㄳ","ㄴ","ㄵ","ㄶ","ㄷ","ㄹ","ㄺ",
+    "ㄻ","ㄼ","ㄽ","ㄾ","ㄿ","ㅀ","ㅁ","ㅂ","ㅄ","ㅅ",
+    "ㅆ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"
+  ];
+
+  const result = [CHO[cho], JUNG[jung]];
+  if (JONG[jong]) result.push(JONG[jong]);
+
+  return result;
+};
