ec92742e5217dc92c0220f569d5ab3b8951e53bf
은정 eunjeong <137691527+enunsnv@users.noreply.github.com>
2026-08-22 17:35:17 +0900

    Update layout.tsx

--- tadak/app/layout.tsx
+++ tadak/app/layout.tsx
@@ -45,6 +45,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5178512088906089"
+     crossorigin="anonymous"></script>
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
